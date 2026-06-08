fcf7854cac75fbf38ae249097ef8f91e01fde245
yuitaa <95457974+yuitaa@users.noreply.github.com>
2026-06-08 11:10:37 +0900

    matchingに極大の部分を採用する

--- internal/matching/matching.go
+++ internal/matching/matching.go
@@ -22,20 +22,30 @@ func MatchTemplate(img *image.RGBA, templates *templates.TemplateMap, matchOnce 
 TemplateLoop:
 	for key, template := range *templates {
 		templateW := template.Bounds().Dx()
+		var bestMatch *ScoreEntry
 
 		for dx := range imgW - templateW {
 			score, _ := calculateScore(img, template, dx)
 
 			if score > threshold {
-				results = append(results, ScoreEntry{
-					Key:   key,
-					Score: score,
-					Dx:    dx,
-				})
+				if bestMatch == nil || dx > bestMatch.Dx+10 {
+					if bestMatch != nil {
+						results = append(results, *bestMatch)
+					}
+					bestMatch = &ScoreEntry{Key: key, Score: score, Dx: dx}
+				} else if score > bestMatch.Score {
+					bestMatch.Score = score
+					bestMatch.Dx = dx
+				}
+
 				if matchOnce {
+					results = append(results, *bestMatch)
 					break TemplateLoop
 				}
 			}
+		}
+		if bestMatch != nil && !matchOnce {
+			results = append(results, *bestMatch)
 		}
 	}
 
