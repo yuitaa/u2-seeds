362235f78020f99e0a90a4d5d9d4c28e88f31898
yuitaa <95457974+yuitaa@users.noreply.github.com>
2026-06-08 10:01:24 +0900

    template

---
+++ internal/templates/templates.go
@@ -0,0 +1,62 @@
+package templates
+
+import (
+	"image"
+	"image/draw"
+	_ "image/png"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+var (
+	BiomeTemplate, _     = loadTemplates("biomes")
+	TaskTemplate, _      = loadTemplates("tasks")
+	ExtensionTemplate, _ = loadTemplates("extensions")
+	WagonTemplate, _     = loadTemplates("wagons")
+)
+
+func loadTemplates(dir string) (map[string]*image.RGBA, error) {
+	imageMap := make(map[string]*image.RGBA)
+	templateDir := filepath.Join("internal/templates", dir)
+
+	files, err := os.ReadDir(templateDir)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+		if ext := filepath.Ext(file.Name()); strings.ToLower(ext) != ".png" {
+			continue
+		}
+
+		path := filepath.Join(templateDir, file.Name())
+		f, err := os.Open(path)
+		if err != nil {
+			continue
+		}
+		defer f.Close()
+
+		template, _, err := image.Decode(f)
+		if err != nil {
+			continue
+		}
+
+		bounds := template.Bounds()
+		rgbaImg := image.NewRGBA(bounds)
+
+		draw.Draw(rgbaImg, bounds, template, bounds.Min, draw.Src)
+
+		imageMap[getFileName(file.Name())] = rgbaImg
+	}
+	return imageMap, nil
+}
+
+func getFileName(path string) string {
+	base := filepath.Base(path)
+	ext := filepath.Ext(base)
+	return strings.TrimSuffix(base, ext)
+}
