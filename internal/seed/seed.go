7b1fa66cc4cbe3f9d3a3b561637457a9d9e3e27e
yuitaa <95457974+yuitaa@users.noreply.github.com>
2026-06-08 16:18:40 +0900

    seed

--- internal/seed/seed.go
+++ internal/seed/seed.go
@@ -1,3 +1,29 @@
 package seed
 
+import (
+	"math/rand"
+	"time"
+)
+
 type SeedString string
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+-"
+const prefix = "U"
+
+type SeedGenerator struct {
+	rng *rand.Rand
+}
+
+func NewSeedGenerator() *SeedGenerator {
+	return &SeedGenerator{
+		rng: rand.New(rand.NewSource(time.Now().UnixNano())),
+	}
+}
+
+func (sg *SeedGenerator) Generate() SeedString {
+	b := make([]byte, 7)
+	for i := range b {
+		b[i] = charset[sg.rng.Intn(len(charset))]
+	}
+	return SeedString(prefix + string(b))
+}
