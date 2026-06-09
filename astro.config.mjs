d12627d105fc7de71a71e7ef3d8684ae21c29374
yuitaa <95457974+yuitaa@users.noreply.github.com>
2026-06-09 15:51:37 +0900

    github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://yuitaa.github.io',
+  base: '/u2-seeds',
+  output: 'static',
+});
